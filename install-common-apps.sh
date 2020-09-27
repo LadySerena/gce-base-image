b2ccc9e78cdafba284982be90c37f2eae5e20117
LadySerena <serena.tiede+github@gmail.com>
2020-09-26 23:21:54 -0500

    feat: add node exporter and gcp agent

--- install-common-apps.sh
+++ install-common-apps.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
-gsutil cp gs://serena-minecraft-platform-debian-repo/node-exporter_0.18.1-1_amd64.deb /tmp/
-sudo apt install /tmp/node-exporter_0.18.1-1_amd64.deb
-rm /tmp/node-exporter_0.18.1-1_amd64.deb
+#!/usr/bin/env bash
+# install node exporter
+gsutil cp gs://debian-v1.platform.serenacodes.com/node-exporter_1.0.1-1_amd64.deb /tmp/
+sudo apt install /tmp/node-exporter_1.0.1-1_amd64.deb
+rm /tmp/node-exporter_1.0.1-1_amd64.deb
+
+#install gcp monitoring agent
+curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+sudo bash add-monitoring-agent-repo.sh
+sudo apt-get update
+sudo apt-cache madison stackdriver-agent
+sudo apt-get install -y 'stackdriver-agent=6.*'
+sudo systemctl enable stackdriver-agent
+sudo systemctl start stackdriver-agent
