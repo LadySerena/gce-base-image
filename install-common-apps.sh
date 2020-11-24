8cad78ac5bc35ba796f2e41b1dcd1d1dc7d8cf24
LadySerena <serena.tiede+github@gmail.com>
2020-11-23 22:17:27 -0600

    feat: add common package for policykit
    software-properties-common has policykit and some other related dbus
    goodies.

--- install-common-apps.sh
+++ install-common-apps.sh
@@ -4,6 +4,9 @@ gsutil cp gs://debian-v1.platform.serenacodes.com/node-exporter_1.0.1-1_amd64.de
 sudo apt install /tmp/node-exporter_1.0.1-1_amd64.deb
 rm /tmp/node-exporter_1.0.1-1_amd64.deb
 
+# install policy kit
+sudo apt-get install software-properties-common -y
+
 #install gcp monitoring agent
 curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
 sudo bash add-monitoring-agent-repo.sh
@@ -11,6 +14,3 @@ sudo apt-get update
 sudo apt-cache madison stackdriver-agent
 sudo apt-get install -y 'stackdriver-agent=6.*' 'lvm2'
 sudo systemctl enable stackdriver-agent
-sudo systemctl start stackdriver-agent
-
-
