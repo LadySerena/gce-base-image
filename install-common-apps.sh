ff92102a091290e5571c691ba137f4357f20a1b5
LadySerena <serena.tiede+github@gmail.com>
2021-01-20 19:56:24 -0600

    changing path for node exporter

--- install-common-apps.sh
+++ install-common-apps.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # install node exporter
-gsutil cp gs://debian-v1.platform.serenacodes.com/node-exporter_1.0.1-1_amd64.deb /tmp/
-sudo apt install /tmp/node-exporter_1.0.1-1_amd64.deb
-rm /tmp/node-exporter_1.0.1-1_amd64.deb
+gsutil cp gs://debian-v1.platform.serenacodes.com/node-exporter_1.0.1-2_amd64.deb /tmp/
+sudo apt install /tmp/node-exporter_1.0.1-2_amd64.deb
+rm /tmp/node-exporter_1.0.1-2_amd64.deb
 
 # install policy kit
 sudo apt-get install software-properties-common -y
