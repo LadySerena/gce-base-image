511d1452087a234c78e37bf5ce6e85ee07acb78a
LadySerena <serena.tiede+github@gmail.com>
2021-10-09 19:45:43 -0500

    feat: replace stackdriver agent with ops agent
    Google rebranded their agent and this image needed to be updated.

--- install-common-apps.sh
+++ install-common-apps.sh
@@ -7,10 +7,9 @@ rm /tmp/node-exporter_1.0.1-2_amd64.deb
 # install policy kit
 sudo apt-get install software-properties-common -y
 
-#install gcp monitoring agent
-curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
-sudo bash add-monitoring-agent-repo.sh
+#install gcp ops agent
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install --verbose
 sudo apt-get update
-sudo apt-cache madison stackdriver-agent
-sudo apt-get install -y 'stackdriver-agent=6.*' 'lvm2'
-sudo systemctl enable stackdriver-agent
+sudo apt-get install -y 'lvm2'
+sudo systemctl enable google-cloud-ops-agent
